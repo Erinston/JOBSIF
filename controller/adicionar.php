afd6cf6112cf4e8d5b12e86c470f766cae11a4b9
jackson29 <37213324+jackson29@users.noreply.github.com>
2019-10-26 23:20:34 -0300

    Update adicionar.php
    tirando linha de comando csv

--- controller/adicionar.php
+++ controller/adicionar.php
@@ -8,31 +8,6 @@ $telefone= $_POST['telefone'];
 $usuario= $_POST['usuario'];
 $pw= $_POST['pw'];
 $funcao= 'usuario';
-
-$file = file('usuario.csv');
-foreach ($file as $coluna => $key):
-	$explode = explode(";/;", $file[$coluna]);
-	if ($cpf == $explode[2]):
-		header("location: ../View/cadastro.php");
-		body("cpf ja existe");
-		break;
-	endif;
-	$id++;
-endforeach;
-
-//Adicionando no arquivo CSV
-$fopen = fopen('../Csv/usuario.csv','a');
-fwrite($fopen,
-	 $id.';/;'
-	.$nome.';/;'
-	.$cpf.';/;'
-	.$matricula.';/;'
-	.$telefone.';/;'
-	.$usuario.';/;'
-	.$pw.';/;'
-	.$funcao."\n");
-fclose($fopen);
-	
 //Redirecionando para o arquivo index.php
 header("location: ../View/login.php");
 
