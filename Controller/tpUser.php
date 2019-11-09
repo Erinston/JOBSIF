667a04ca022e075db9ae0247e51e3716a7d8f840
Erinston <49694337+Erinston@users.noreply.github.com>
2019-11-09 09:27:22 -0300

    Delete tpUser.php

--- Controller/tpUser.php
+++
@@ -1,9 +0,0 @@
-<?php 
-require('conexao.php');
-$u = "professor";
-$sql = "INSERT INTO tipos_user(NOME) VALUES(:u)";
-$query =$conn->prepare($sql);
-$query->bindParam(":u",$u);
-$query->execute(); 
-
- ?>
