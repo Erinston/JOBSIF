5d57b749b870119950495360dbe11f37a7c23d9b
Erinston <49694337+Erinston@users.noreply.github.com>
2019-11-09 08:35:11 -0300

    sem necessidade

--- view/teste.sql
+++
@@ -1,1 +0,0 @@
-CREATE TABLE `user`.`dados` ( `cpf` INT(11) NOT NULL , `nome` VARCHAR(45) NOT NULL , `usuario` VARCHAR(45) NOT NULL , `telefone` INT(11) NOT NULL , `funcao` VARCHAR(45) NOT NULL , `matricula` TEXT NOT NULL , PRIMARY KEY (`cpf`) ENGINE = InnoDB;
